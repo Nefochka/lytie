5060f806b1de4d23ac14bba9b1fe15d27528328e
Oxana Goutnik <og@syrec.org>
2013-10-26 08:57:13 -0500

    russian buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  def facebook_like_button_settings
+    {
+      'data-href' => "http://lytie.herokuapp.com/",
+      'data-width' => "90",
+      'data-height' => "20",
+      'data-colorscheme' => "light",
+      'data-layout' => "button_count",
+      'data-action' => "recommend",
+      'data-show-faces' => "false",
+      'data-send' => "false"
+    }
+  end
+
+  def twitter_button_settings
+    {
+        :href=>"https://twitter.com/share",
+        :class=>"twitter-share-button",
+        :url=>"http://lytie.herokuapp.com/",
+        :related=>"jasoncosta",
+        :lang=>"ru",
+        :size=>"large",
+        :count=>"none"
+    }
+  end
 end
