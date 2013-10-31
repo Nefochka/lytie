d1ffdf672b5a1ccf73403b3830f720e5826e9b7f
Nefochka <nefirtity@mail.ru>
2013-10-31 19:07:40 +0400

    commit 18

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-= require turbolinks
 //= require_tree .
+
+
+$('document').on 'click', '.edit_task input[type=checkbox]', ->
+    $(this).parent('form').submit()
